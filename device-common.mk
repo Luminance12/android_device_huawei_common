9f9e02f8d7f9ebc6d5670d8feaccd3e893b7c28c
Luminance12 <37965929+Luminance12@users.noreply.github.com>
2018-09-10 15:10:09 +0200

    Update device-common.mk

--- device-common.mk
+++ device-common.mk
@@ -18,7 +18,7 @@ $(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/treble_common.mk)
-$(call inherit-product, vendor/huawei/vndk/vndk.mk)
+$(call inherit-product, vendor/huawei/common/huawei-vendor.mk)
 
 # Platform Path
 PLATFORM_PATH := device/huawei/common
